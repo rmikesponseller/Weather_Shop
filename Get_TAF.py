624ad72debfd7702d2f8e6165417c2b6dd28a09f
rmikesponseller <44551158+rmikesponseller@users.noreply.github.com>
2019-11-09 19:06:27 -0600

    Update Get_TAF.py

--- Get_TAF.py
+++ Get_TAF.py
@@ -6,7 +6,7 @@ r = requests.get('https://www.aviationweather.gov/taf/data?ids=KDLF&format=raw&m
 print(r.text[0:500])
 soup = BeautifulSoup(r.text,'html.parser')
 
-results = soup.find_all('span', attrs{'class':'short-desc'})
+results = soup.find_all('span', attrs={'class':'short-desc'})
 len(results)
 print(results[0:3])
 

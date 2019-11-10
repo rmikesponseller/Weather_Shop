69f4feddda5918f67f95ac81d3ff5ec3f89cc62f
rmikesponseller <44551158+rmikesponseller@users.noreply.github.com>
2019-11-09 19:19:34 -0600

    Update Get_TAF.py

--- Get_TAF.py
+++ Get_TAF.py
@@ -3,13 +3,15 @@ from bs4 import BeautifulSoup
 
 r = requests.get('https://www.aviationweather.gov/taf/data?ids=KDLF&format=raw&metars=on&date=&submit=Get+TAF+data')
 
-print(r.text[0:500])
+r.text[0:500]
 soup = BeautifulSoup(r.text,'html.parser')
 
 results = soup.find_all('span', attrs={'class':'short-desc'})
 len(results)
 first_result = results[0]
 first_result.find('strong')
+
+
 
 
 

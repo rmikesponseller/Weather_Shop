61804222cb0349ef5e8ec4b489dd3c3227d1fedb
rmikesponseller <44551158+rmikesponseller@users.noreply.github.com>
2019-11-09 19:54:44 -0600

    Update Get_TAF.py

--- Get_TAF.py
+++ Get_TAF.py
@@ -5,15 +5,22 @@ r = requests.get('https://www.aviationweather.gov/taf/data?ids=KSAT&format=raw&m
 
 soup = BeautifulSoup(r.text,'html.parser')
 
-date_time_1 = soup.find_all('strong')
-for entry in date_time_1:
-	date_time = date_time_1.find('strong').text
-	print(date_time)
+date_results = soup.find_all('strong')
+date_time_records = []
 
-TAF_1 = soup.find_all('code')
-for entry in TAF_1:
-	TAF = TAF_1.find('code').text
-	print(TAF)
+for result in date_results:
+	date_time = result.find('strong').text
+	data_time_records.append(date_time)
+
+TAF_results = soup.find_all('code')
+TAF_records = []
+
+for result in TAF_results:
+	TAF = result.find('code').text
+	TAF_records.append(TAF)
+
+print(data_time_records)
+print(TAF_records)
 
 
 

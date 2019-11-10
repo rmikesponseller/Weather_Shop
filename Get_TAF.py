8f2e67131fff81df3cacf3d33262bad8b241166f
rmikesponseller <44551158+rmikesponseller@users.noreply.github.com>
2019-11-09 19:03:03 -0600

    Add files via upload

---
+++ Get_TAF.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get('https://www.aviationweather.gov/taf/data?ids=KDLF&format=raw&metars=on&date=&submit=Get+TAF+data')
+
+print(r.text[0:500])
+soup = BeautifulSoup(r.text,'html.parser')
+
+results = soup.find_all('span', attrs{'class':'short-desc'})
+len(results)
+print(results[0:3])
+
